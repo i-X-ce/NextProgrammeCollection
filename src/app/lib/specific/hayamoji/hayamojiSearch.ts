73415fdccd29ecdc9e9e2b32cd542a810c9cada1
i-X-ce <iceci2804@gmail.com>
2025-01-21 12:06:58 +0900

    ✨ ひらがなカタカナの判定とか

--- src/app/lib/specific/hayamoji/hayamojiSearch.ts
+++ src/app/lib/specific/hayamoji/hayamojiSearch.ts
@@ -1,22 +1,141 @@
-import TinyQueue from "tinyqueue";
 import { mod } from "../../common/calc";
 import { ButtonType } from "./ButtonType";
-import { reverse } from "dns";
+import { pokemoji } from "../../common/pokemoji";
+import { isKatakana, toHiragana, toKatakana } from "wanakana";
 
 export class HayamojiSearch {
   private map: HayamojiMap = new HayamojiMap();
+  static readonly DAKUTEN_CONVERSION: { [key: string]: string } = {
+    カ: "ガ",
+    キ: "ギ",
+    ク: "グ",
+    ケ: "ゲ",
+    コ: "ゴ",
+    サ: "ザ",
+    シ: "ジ",
+    ス: "ズ",
+    セ: "ゼ",
+    ソ: "ゾ",
+    タ: "ダ",
+    チ: "ヂ",
+    ツ: "ヅ",
+    テ: "デ",
+    ト: "ド",
+    ハ: "バ",
+    ヒ: "ビ",
+    フ: "ブ",
+    ヘ: "ベ",
+    ホ: "ボ",
+    か: "が",
+    き: "ぎ",
+    く: "ぐ",
+    け: "げ",
+    こ: "ご",
+    さ: "ざ",
+    し: "じ",
+    す: "ず",
+    せ: "ぜ",
+    そ: "ぞ",
+    た: "だ",
+    ち: "ぢ",
+    つ: "づ",
+    て: "で",
+    と: "ど",
+    は: "ば",
+    ひ: "び",
+    ふ: "ぶ",
+    へ: "べ",
+    ほ: "ぼ",
+  };
+  static readonly HANDAKUTEN_CONVERSION: { [key: string]: string } = {
+    ハ: "パ",
+    ヒ: "ピ",
+    フ: "プ",
+    ヘ: "ペ",
+    ホ: "ポ",
+    は: "ぱ",
+    ひ: "ぴ",
+    ふ: "ぷ",
+    へ: "ぺ",
+    ほ: "ぽ",
+  };
 
-  public search(start: string, goal: string) {
-    return this.map.bfsSearch(start, goal);
+  public search(value: string) {
+    const array = this.string2array(value);
+    if (!array) return null;
+    const charList = ["ア"].concat(array);
+
+    let name = "";
+    let ret = [];
+    let iskata = true;
+    for (let i = 0; i < charList.length - 1; i++) {
+      const startChar = charList[i];
+      const endChar = charList[i + 1];
+      let changedType = false;
+      if (endChar !== "゛" && endChar !== "゜") {
+        const preKata: boolean = iskata;
+        iskata = isKatakana(endChar);
+        if (preKata != iskata) changedType = true;
+      }
+
+      const result = this.map.bfsSearch(
+        startChar === "En" ? "En" : toKatakana(startChar),
+        toKatakana(endChar),
+        iskata ? "katakana" : "hiragana",
+        changedType
+      );
+      if (endChar === "゛" || endChar === "゜") {
+        name = name.substring(0, name.length - 1);
+        if (endChar === "゛") {
+          name += HayamojiSearch.DAKUTEN_CONVERSION[charList[i]] || "";
+        } else {
+          name += HayamojiSearch.HANDAKUTEN_CONVERSION[charList[i]] || "";
+        }
+      } else {
+        name += endChar;
+      }
+      // console.log(startChar, endChar, result);
+      if (!result) return null;
+      ret.push(result.path);
+    }
+    console.log(name);
+    return ret;
   }
 
-  private string2array(str: string): string[] {
-    let ret = str.split("").map((s) => {});
+  private string2array(str: string): string[] | null {
+    const formatRIHEBEPE: { [key: string]: string } = {
+      り: "リ",
+      へ: "ヘ",
+      べ: "ベ",
+      ぺ: "ペ",
+    };
+
+    let spl = str.split("");
+    spl = spl.map((s) => formatRIHEBEPE[s] || s);
+    if (
+      spl.length !==
+      spl.filter((s) => {
+        const p = pokemoji.find((p) => p.char === s);
+        return p && p.possible;
+      }).length
+    )
+      return null;
+
+    let ret = [];
+    for (let i = 0; i < spl.length; i++) {
+      const p = pokemoji.find((p) => p.char === spl[i])!;
+      if (p.dakuten) {
+        ret.push(p.initchar);
+        ret.push(p.dakuten);
+      } else ret.push(p.char);
+    }
+
+    return ret;
   }
 }
 
 class HayamojiMap {
-  static readonly chars = [
+  static readonly CHARS = [
     ["ア", "イ", "ウ", "エ", "オ", "ャ"],
     ["カ", "キ", "ク", "ケ", "コ", "ュ"],
     ["サ", "シ", "ス", "セ", "ソ", "ョ"],
@@ -31,18 +150,18 @@ class HayamojiMap {
   private nodeMap: Map<string, Node> = new Map();
 
   constructor() {
-    this.map = new Array(HayamojiMap.chars.length);
-    for (let i = 0; i < HayamojiMap.chars.length; i++) {
-      this.map[i] = new Array(HayamojiMap.chars[i].length);
-      for (let j = 0; j < HayamojiMap.chars[i].length; j++) {
-        this.map[i][j] = new Node(HayamojiMap.chars[i][j]);
-        this.nodeMap.set(HayamojiMap.chars[i][j], this.map[i][j]);
+    this.map = new Array(HayamojiMap.CHARS.length);
+    for (let i = 0; i < HayamojiMap.CHARS.length; i++) {
+      this.map[i] = new Array(HayamojiMap.CHARS[i].length);
+      for (let j = 0; j < HayamojiMap.CHARS[i].length; j++) {
+        this.map[i][j] = new Node(HayamojiMap.CHARS[i][j]);
+        this.nodeMap.set(HayamojiMap.CHARS[i][j], this.map[i][j]);
       }
     }
 
     const kana: Node = new Node("かな");
-    for (let i = 0; i < HayamojiMap.chars.length; i++) {
-      for (let j = 0; j < HayamojiMap.chars[i].length; j++) {
+    for (let i = 0; i < HayamojiMap.CHARS.length; i++) {
+      for (let j = 0; j < HayamojiMap.CHARS[i].length; j++) {
         const target: Node = this.map[i][j];
         if (j > 0) {
           target.addChild(ButtonType.Up, this.map[i][j - 1]);
@@ -52,9 +171,9 @@ class HayamojiMap {
         }
         target.addChild(
           ButtonType.Left,
-          this.map[mod(i - 1, HayamojiMap.chars.length)][j]
+          this.map[mod(i - 1, HayamojiMap.CHARS.length)][j]
         );
-        this.map[mod(i - 1, HayamojiMap.chars.length)][j].addChild(
+        this.map[mod(i - 1, HayamojiMap.CHARS.length)][j].addChild(
           ButtonType.Right,
           target
         );
@@ -66,7 +185,9 @@ class HayamojiMap {
 
   public bfsSearch(
     start: string,
-    goal: string
+    goal: string,
+    type: "katakana" | "hiragana",
+    changedType: boolean
   ): {
     cost: number;
     path: { char: string; button: ButtonType }[];
@@ -132,15 +253,29 @@ class HayamojiMap {
       {
         let target: VisitedData = visitedData.get(goalNode)!;
         if (!target) return null;
-        let ret: { char: string; button: ButtonType }[] = [];
+        let path: { char: string; button: ButtonType }[] = [];
         while (target.predata) {
-          ret.push({ char: target.node!.c, button: target.predata.button! });
+          path.push({
+            char: target.node!.char,
+            button: target.predata.button!,
+          });
           target = visitedData.get(target.predata.node)!;
         }
         // ret.push({ char: start, button: ButtonType.A });
+
+        const formatKana = (s: string) => {
+          if (type === "katakana") return toKatakana(s);
+          else return toHiragana(s);
+        };
+        path = path.map((p) => {
+          return { char: formatKana(p.char), button: p.button };
+        });
+        if (changedType) path.push({ char: start, button: ButtonType.Select });
+        path = path.reverse();
+        path.push({ char: formatKana(goal), button: ButtonType.A });
         return {
           cost: visitedData.get(goalNode)?.cost || -1,
-          path: ret.reverse(),
+          path,
         };
       }
     };
@@ -154,7 +289,7 @@ class HayamojiMap {
 
 class Node {
   public children: { button: ButtonType; node: Node }[] = [];
-  constructor(public c: string) {}
+  constructor(public char: string) {}
 
   public addChild(button: ButtonType, node: Node) {
     this.children.push({ button, node });
--- src/app/pages/hayamoji/page.tsx
+++ src/app/pages/hayamoji/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 import { HayamojiSearch } from "@/app/lib/specific/hayamoji/hayamojiSearch";
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchText, setSearchText] = useState("");
   const hayamojisearch = new HayamojiSearch();
   return (
     <>
-      <TextField />
+      <TextField
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+        value={searchText}
+      />
       <div>
-        {hayamojisearch.search("ア", "ロ")?.path.map((s, i) => (
-          <p key={i}>{s.char}</p>
+        {hayamojisearch.search(searchText)?.map((s, i) => (
+          <p key={i}>
+            {s.map((ss) => (
+              ss.char
+            ))}
+          </p>
         ))}
       </div>
     </>

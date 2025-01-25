05eaa325e1df0966b8640fb9b48cb63fad82f990
i-X-ce <iceci2804@gmail.com>
2025-01-25 19:23:40 +0900

    🎨 名前とファビコンの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href="./pages/hayamoji">はやじもじ</Link>
+      <Link href="./pages/hayamoji">はやもじ</Link>
     </>
   );
 }
